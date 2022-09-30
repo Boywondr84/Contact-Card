bcfd4139a751e207d8555fc3d3ff0ea3952e0850
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-09-30 16:05:26 -0500

    added idb package. logic for initDB added. trouble connecting to server. no errors when running build

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -8,6 +8,8 @@ import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { initDB } from "./database.js";
+
 // import src/images files
 // import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
@@ -15,7 +17,9 @@ import Dog from "../images/dog.png";
 import Bird from "../images/bird.jpg";
 
 window.addEventListener('load', function() {
+    initDB();
     document.getElementById('logo').src = Bird;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
+
 });

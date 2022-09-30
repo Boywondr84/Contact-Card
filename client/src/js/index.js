2cc32aee6e233e18b872571ad8f148bd6aaf9b72
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-09-30 12:06:08 -0500

    added htmlwebpack plugin, style loader babel loader css loader

---
+++ client/src/js/index.js
@@ -0,0 +1,21 @@
+// import src/js files
+import "./form";
+import "./submit";
+
+// import src/css files
+import "../css/index.css";
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import src/images files
+// import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
+import Bird from "../images/bird.jpg";
+
+window.addEventListener('load', function() {
+    document.getElementById('logo').src = Bird;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+});

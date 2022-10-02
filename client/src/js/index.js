68e7c8880bd2966976785c435611bf2cc0ad0c35
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-10-02 00:43:09 -0500

    import toggleForm and clearForm. some logic changes. everything working!

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,8 @@
 // import src/js files
 import "./form";
-import "./submit";
+// import "./submit";
+import "./card";
+import { toggleForm, clearForm } from "./form";
 
 // import src/css files
 import "../css/index.css";
@@ -14,15 +16,50 @@ import { initDB, getDb, postDb } from "./database.js";
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
+import { fetchCards } from "./card";
 // import Bird from "../images/bird.jpg";
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     initDB();
-    getDb();
-    postDb();
-    getDb();
+    fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 
+});
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+    toggleForm()
+})
+
+form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
+
+        fetchCards();
+        // Toggles the submit button back to POST functionality
+        submitBtnToUpdate = false;
+    }
+
+    // Clear form
+    clearForm();
+    // Toggle form
+    toggleForm();
+    // Reload the DOM
+    fetchCards();
 });
